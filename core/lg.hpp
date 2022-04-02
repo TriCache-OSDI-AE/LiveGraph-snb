ed08d41c4f54e6230f0834a16627d3cde0558dac
Guanyu Feng <jiguanglizipao@gmail.com>
2022-04-02 10:16:35 +0800

    add USE_SHARED_LIVEGRAPH

--- core/lg.hpp
+++ core/lg.hpp
@@ -1,4 +1,13 @@
 #pragma once
 
+#ifdef USE_SHARED_LIVEGRAPH
+
 #include "LiveGraph/bind/livegraph.hpp"
 using namespace lg;
+
+#else
+
+#include "LiveGraph/core/livegraph.hpp"
+using namespace livegraph;
+
+#endif
